1967fde2a81785c6c73463fd7d5c2bfde0da39b1
petekung2000 <peach@yojoies.com>
2023-07-04 17:42:15 +0700

    #

--- backend/App.js
+++ backend/App.js
@@ -15,44 +15,79 @@ const connection = mysql.createConnection({
 });
 app.use(cors())
 app.post('/register', jsonParser, function (req, res, next) {
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
         connection.query(
-            'INSERT INTO users (email,password,fname,lname) VALUES (?,?,?,?)',[req.body.email,hash,req.body.fname,req.body.lname],
-            function (err, results, fields) {
-                if(err){
-                    res.json({status:'Error' ,message:err})
-                    return
+            'SELECT * FROM users WHERE  email  = ?  ', [req.body.email],
+            function (err, users, fields) {
+                if (err) {
+                    res.json({ status: 'Error', message: err })
+                    console.log(fields);
                 }
-                res.json({ status:'Sucess' })
+                if(users.length == 0 ){
+                    connection.query(
+                        'INSERT INTO users (email,password,fname,lname) VALUES (?,?,?,?)', [req.body.email, hash, req.body.fname, req.body.lname],
+                        function (err, results, fields) {
+                            if (err) {
+                                res.json({ status: 'Error', message: err })
+                                return
+                            }
+            
+                            res.json({ status: 'Sucess' })
+                        }
+                    );    
+                }
+                if(users.length != 0){
+                    res.json({ status: 'Error', message: 'Users Already' })
+
+                }
+           
+    
+    
             }
         );
+  
     });
 
 })
 app.post('/login', jsonParser, function (req, res, next) {
     connection.query(
-        'SELECT * FROM users WHERE email = ?',[req.body.email],
+        'SELECT * FROM users WHERE  email  = ?  ', [req.body.email],
         function (err, users, fields) {
-            if(err){
-                res.json({status:'Error' ,message:err}) 
-                return
+            if (err) {
+                res.json({ status: 'Error', message: err })
+                console.log(fields);
             }
-            if(users.length == 0){
-                res.json({status:'Error' ,message:'No user found'}) 
-            }
-            bcrypt.compare(req.body.password, users[0].password, function(err, isLogin) {
-                if(isLogin){
-                    res.json({status:'Sucess' ,message:'Login Sucess '})
-                }else{
-                    res.json({status:'Error' ,message:'Login Failed '}) 
+            if(users.length == 0 ){
+                res.json({ status: 'Error', message: 'No users found ' })
 
-                }
-            });
-         
+            }
+            if (users.length !=0) {
+                bcrypt.compare(req.body.password, users[0].password, function (err, isLogin) {
+
+                    if (isLogin) {
+
+                        var token = jwt.sign({ email: users[0].email }, secret);
+                        res.json({ status: 'Sucess', message: 'Login Sucess ', token }) 
+                    } else {
+                        res.json({ status: 'Error', message: 'Password is incorrect ' })
+
+                    }
+                 
+
+                });
+            }
+
+
         }
     );
 
 })
+app.post('/Authen', jsonParser, function (req, res, next) {
+
+
+
+
+})
 app.listen(3333, jsonParser, function () {
-    console.log('CORS-enabled web server listening on port 3333')
+    console.log('Server Runing on port 3333')
 })
